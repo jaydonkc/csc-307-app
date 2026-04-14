9a0d737b9dfad103222e71765fa0e3f71c4b4eca
BoB121isawesome <79879038+BoB121isawesome@users.noreply.github.com>
2026-04-14 09:36:09 -0700

    Add Express backend users API

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,11 +3,87 @@ import express from "express";
 
 const app = express();
 const port = 8000;
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
 
 app.use(express.json());
 
+const findUserByName = (name) => {
+  return users.users_list.filter((user) => user.name === name);
+};
+
+const findUserById = (id) => {
+  return users.users_list.find((user) => user.id === id);
+};
+
+const addUser = (user) => {
+  users.users_list.push(user);
+  return user;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+
+  if (name !== undefined) {
+    const result = {
+      users_list: findUserByName(name),
+    };
+
+    res.send(result);
+    return;
+  }
+
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+
+  addUser(userToAdd);
+  res.send();
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const result = findUserById(id);
+
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+    return;
+  }
+
+  res.send(result);
 });
 
 app.listen(port, () => {
